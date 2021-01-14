3eea83b71ae80fce2450e35618408b5424cf6ba7
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-14 22:04:35 +0100

    One to Many JPA Relationship

--- src/main/java/com/fastus/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/fastus/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.fastus.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by Tom - 14.01.2021
@@ -22,11 +23,22 @@ public class Recipe {
     //todo add
     //private Difficulty difficulty;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     @Lob
     private Byte [] image;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 
     public String getDescription() {
         return description;
