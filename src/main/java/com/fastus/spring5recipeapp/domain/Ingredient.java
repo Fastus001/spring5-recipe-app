9d3cebc5f49b963e69ff6feadcfbb12ada1599c8
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-14 22:14:53 +0100

    Assignment One to One JPA Persistence
    UnitOfMeasure class

--- src/main/java/com/fastus/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/fastus/spring5recipeapp/domain/Ingredient.java
@@ -12,10 +12,13 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    //in one to one should be by default ;-)
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -50,5 +53,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
