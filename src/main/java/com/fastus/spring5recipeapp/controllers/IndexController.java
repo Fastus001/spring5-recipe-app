1341833a17004694538e5a72f1523fbe1b5a913f
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-17 21:06:22 +0100

    Assignment - display list of Recipes

--- src/main/java/com/fastus/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/fastus/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,9 @@
 package com.fastus.spring5recipeapp.controllers;
 
-import com.fastus.spring5recipeapp.domain.Category;
-import com.fastus.spring5recipeapp.domain.UnitOfMeasure;
-import com.fastus.spring5recipeapp.repositories.CategoryRepository;
-import com.fastus.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import com.fastus.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * Created by Tom - 21.12.2020
@@ -16,24 +11,15 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat Id is:" + categoryOptional.get().getId());
-        System.out.println("UOM Id is:" + unitOfMeasure.get().getId());
-
-
+        model.addAttribute("recipes",recipeService.getRecipes());
 
         return "index";
     }
---
+++ src/main/java/com/fastus/spring5recipeapp/services/RecipeService.java
@@ -0,0 +1,13 @@
+package com.fastus.spring5recipeapp.services;
+
+import com.fastus.spring5recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by Tom - 17.01.2021
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
