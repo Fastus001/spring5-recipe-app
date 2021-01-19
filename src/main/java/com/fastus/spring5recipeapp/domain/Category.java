8689a61435ab894534c020b05882b84ef980f26a
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-19 21:47:05 +0100

    Using Project Lombok

--- src/main/java/com/fastus/spring5recipeapp/domain/Category.java
+++ src/main/java/com/fastus/spring5recipeapp/domain/Category.java
@@ -1,11 +1,14 @@
 package com.fastus.spring5recipeapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
 /**
  * Created by Tom - 14.01.2021
  */
+@Data
 @Entity
 public class Category {
 
@@ -17,27 +20,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
--- src/main/java/com/fastus/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/fastus/spring5recipeapp/domain/Recipe.java
@@ -1,5 +1,7 @@
 package com.fastus.spring5recipeapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,6 +9,7 @@ import java.util.Set;
 /**
  * Created by Tom - 14.01.2021
  */
+@Data
 @Entity
 public class Recipe {
 
@@ -51,74 +54,6 @@ public class Recipe {
         this.ingredients = ingredients;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getPrepTime() {
-        return prepTime;
-    }
-
-    public void setPrepTime(Integer prepTime) {
-        this.prepTime = prepTime;
-    }
-
-    public Integer getCookTime() {
-        return cookTime;
-    }
-
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    public Integer getServings() {
-        return servings;
-    }
-
-    public void setServings(Integer servings) {
-        this.servings = servings;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getDirections() {
-        return directions;
-    }
-
-    public void setDirections(String directions) {
-        this.directions = directions;
-    }
-
-    public Byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public Notes getNotes() {
-        return notes;
-    }
-
     public void setNotes(Notes notes) {
         this.notes = notes;
         notes.setRecipe(this);
@@ -130,27 +65,4 @@ public class Recipe {
         return this;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
-    }
 }
