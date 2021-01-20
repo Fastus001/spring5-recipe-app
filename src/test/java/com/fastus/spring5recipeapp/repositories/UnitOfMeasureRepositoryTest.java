a8ea9c14a81ee447688fa60254f6ee979847725f
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-20 23:16:02 +0100

    Spring integration test

---
+++ src/test/java/com/fastus/spring5recipeapp/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.fastus.spring5recipeapp.repositories;
+
+import com.fastus.spring5recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon",unitOfMeasureOptional.get().getDescription());
+    }
+}
