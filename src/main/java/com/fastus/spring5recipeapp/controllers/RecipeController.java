4126761c65f29dbc58e10b422fa4a453ae74f6d7
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-26 23:15:21 +0100

    198. Create Recipe

--- src/main/java/com/fastus/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/fastus/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.fastus.spring5recipeapp.controllers;
 
+import com.fastus.spring5recipeapp.commands.RecipeCommand;
 import com.fastus.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -27,4 +30,20 @@ public class RecipeController {
 
         return "recipe/show";
     }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/"+ savedCommand.getId();
+    }
+
 }
