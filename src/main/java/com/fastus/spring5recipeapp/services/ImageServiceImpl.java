83a7262c0c0d8b509e43b69f653cd314118c5af9
Fastus001 <tkarczmarzyk@gmail.com>
2021-02-02 21:59:58 +0100

    201. Persisiting images to Database

--- src/main/java/com/fastus/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/fastus/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,8 +1,13 @@
 package com.fastus.spring5recipeapp.services;
 
+import com.fastus.spring5recipeapp.domain.Recipe;
+import com.fastus.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by Tom - 02.02.2021
@@ -11,8 +16,36 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl implements ImageService{
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObject = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObject[i++] = b;
+            }
+
+            recipe.setImage(byteObject);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occured", e);
+
+
+            e.printStackTrace();
+        }
     }
 }
